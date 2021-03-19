e1bf00f006ea73cd6d4c9e5fc6b62db992a45819
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2021-03-19 18:42:02 +0530

    Create detect and remove the cycle in the linked list.cpp

---
+++ detect and remove the cycle in the linked list.cpp
@@ -0,0 +1,36 @@
+ public:
+    void removeLoop(Node* head)
+    {
+        Node *slow=head;
+        Node *fast = head;
+        if(head == NULL || head->next == NULL)
+           return;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        if(slow==head){
+            while(slow->next!=head)
+            {
+               slow=slow->next;
+            }
+            slow->next = NULL;
+        }
+        if(slow == fast){
+          slow = head;
+        while(slow->next != fast->next)
+        {
+          if(slow == fast->next){
+           fast->next == NULL;
+        }
+          slow = slow->next;
+          fast = fast->next;
+        }
+        fast->next=NULL;
+    
+        }  
+ 
+    }      

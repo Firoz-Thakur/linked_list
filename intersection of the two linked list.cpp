72af5d436ab76e35af227befb761f148a53c0532
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2021-03-20 14:28:05 +0530

    Create intersection of the two linked list.cpp

---
+++ intersection of the two linked list.cpp
@@ -0,0 +1,42 @@
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* first=head1;
+    Node* second=head2;
+    Node* cor=NULL;
+    Node* newcore;
+    if(first==NULL || second==NULL)
+     return NULL;
+    
+    
+    while(first!=NULL and second!=NULL)
+    {
+       if(first->data==second->data)
+       {
+           Node* temp=new Node(first->data);
+           if(cor==NULL)
+           {
+               cor=temp;
+               newcore=cor;
+           }
+           else
+           {
+               cor->next=temp;
+               cor=cor->next;
+           }
+           first=first->next;
+           second=second->next;
+       }
+       else if((first->data)<(second->data))
+       {
+           first=first->next;
+       }
+       else
+       {
+          second=second->next;  
+       }
+    }
+    return newcore;
+    
+}

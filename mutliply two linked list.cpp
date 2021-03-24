9e8568d17c3134311ab13ae1e46b4bce70f8f12b
Firoz Thakur <56572016+Firoz-Thakur@users.noreply.github.com>
2021-03-24 17:37:10 +0530

    Update mutliply two linked list.cpp

--- mutliply two linked list.cpp
+++ mutliply two linked list.cpp
@@ -1,25 +1,21 @@
 https://practice.geeksforgeeks.org/problems/multiply-two-linked-lists/1
-
-long long  multiplyTwoLists (Node* l1, Node* l2)
+long long multiplyTwoLists (node* first, node* second)
 {
-  //Your code here
-  long long int N=1000000007;
-  long long int num1=0;
-  long long int num2=0;
-  while(l1 || l2)
-  {
-      if(l1)
-      {
-          num1=(num1*10)+l1->data;
-          l1=l1->next;
-      }
-       if(l2)
-      {
-          num1=(num1*10)+l2->data;
-          l2=l2->next;
-      }
-      
-  }
-  return num1*num2;
- 
+//Your code here
+long long N= 1000000007;
+long long num1 = 0, num2 = 0;
+while (first || second)
+{
+if(first){
+num1 = ((num1%N)*10)%N + first->data;
+first = first->next;
+}
+if(second)
+{
+num2 = ((num2%N)*10)%N + second->data;
+second = second->next;
+}
+}
+
+return ((num1%N)*(num2%N))%N;
 }
